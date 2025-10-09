f39d4877155b83c5a7dda88f56a5b83cfa9357b7
yousiefaz <yousiefaboalyazed@gmail.com>
2025-10-10 02:13:43 +0300

    asd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="selection:bg-primary selection:text-white">
+    <html lang="en" className="selection:bg-primary selection:text-white ">
       <body
         className={cn(
           "relative text-muted-foreground",
