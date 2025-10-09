44e4e78d66d17245bfc4ad4468a3d68e25e0c077
yousiefaz <yousiefaboalyazed@gmail.com>
2025-10-10 00:06:52 +0300

    asd

--- src/containers/contact.tsx
+++ src/containers/contact.tsx
@@ -6,28 +6,27 @@ import React from "react";
 
 const ContactSection = () => {
   return (
-    <section
-      id="contact"
-      className="relative container bg-card bg-opacity-80 backdrop-blur-md border rounded-xl p-6 flex flex-col items-center gap-y-6 md:gap-y-12"
-    >
-      <Effect
-        variant="ball"
-        size="default"
-        opacity={0.4}
-        animateIn={false}
-        className="-top-4 -end-4 animate-float-3"
-      />
-      <SectionTitle
-        title="get in touch"
-        description="have an idea? let's talk."
-      />
-      <a
-        href="mailto:yousiefaboalyazed@gmail.com"
-        className={buttonVariants({ size: "lg" })}
-      >
-        <Mail className="size-4" />
-        <span>email me</span>
-      </a>
+    <section id="contact" className="relative container py-20">
+      <div className="bg-card bg-opacity-80 backdrop-blur-md border rounded-xl p-6 flex flex-col items-center gap-y-6 md:gap-y-12">
+        <Effect
+          variant="ball"
+          size="default"
+          opacity={0.4}
+          animateIn={false}
+          className="-top-4 -end-4 animate-float-3"
+        />
+        <SectionTitle
+          title="get in touch"
+          description="have an idea? let's talk."
+        />
+        <a
+          href="mailto:yousiefaboalyazed@gmail.com"
+          className={buttonVariants({ size: "lg" })}
+        >
+          <Mail className="size-4" />
+          <span>email me</span>
+        </a>
+      </div>
     </section>
   );
 };
