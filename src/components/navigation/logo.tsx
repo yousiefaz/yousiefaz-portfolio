b858d6a020122a09b255f38c5079eb2eb03a3b8a
yousiefaz <yousiefaboalyazed@gmail.com>
2025-11-07 01:14:22 +0200

    asd

--- src/components/navigation/logo.tsx
+++ src/components/navigation/logo.tsx
@@ -7,16 +7,12 @@ const Logo = () => {
   return (
     <Link href="/" className="flex items-center gap-3 group">
       <YazLogo
-        className="size-10 fill-black dark:fill-white 
-                   transition-colors duration-2000 ease-in-out 
-                   group-hover:fill-primary dark:group-hover:fill-primary/80"
-        title="I made this logo myself!"
+        className="size-12 fill-black dark:fill-white transition-colors duration-1500 ease-in-out group-hover:fill-primary dark:group-hover:fill-primary/80"
+        title="I made it myself!"
       />
 
       {/* <h1
-        className="font-bold text-2xl capitalize text-black dark:text-white 
-                   transition-colors duration-2000 ease-in-out 
-                   group-hover:text-primary dark:group-hover:text-primary/80"
+        className="font-bold text-2xl capitalize text-black dark:text-white transition-colors duration-2000 ease-in-out group-hover:text-primary dark:group-hover:text-primary/80"
         title="Aka Yaz"
       >
         yousiefAZ
